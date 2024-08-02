f7ce2b451a0f9a8c0b8a066d03082b6a6e5c92f9
VietHoang2000 <20050032@student.bdu.edu.vn>
2024-08-02 14:01:03 +0700

    WebApp Weather Forecast

--- app.py
+++ app.py
@@ -4,26 +4,17 @@ import requests
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/weather', methods=['POST'])
 def weather():
-    city = request.form['city']
-    api_key = 'b040117e1a49b4572f8a7a23c15c5a5f'
-    base_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'
-    response = requests.get(base_url.format(city, api_key)).json()
-
-    if response['cod'] == 200:
-        weather_data = {
-            'city': response['name'],
-            'temperature': response['main']['temp'],
-            'description': response['weather'][0]['description'],
-            'icon': response['weather'][0]['icon'],
-        }
-        return render_template('index.html', weather=weather_data)
-    else:
-        return render_template('index.html', error='City not found')
+    city = request.form.get('city')
+    api_key = 'b040117e1a49b4572f8a7a23c15c5a5f'  # Replace with your OpenWeatherMap API key
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    response = requests.get(url)
+    weather_data = response.json()
+    return render_template('weather.html', weather=weather_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
